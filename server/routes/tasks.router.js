bdafe413933719613ed72855470dcdb51fa2607a
RachelSmith127 <62163543+RachelSmith127@users.noreply.github.com>
2020-08-09 09:47:32 -0500

    Added Get and Post

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -12,36 +12,43 @@ const pool = new Pool({
 });
 
 //let's us know if we have connected 
-pool.on('connect', () =>{
-    console.log('pg connected!');
-});
-
-pool.on('error', (error) =>{
-    console.log('error with pool', error)
-});
-
-// router.get('/', (req, res) => {
-//     let queryText = `SELECT * FROM "tasks";`
-//     //go to db
-//     //get the tasks
-//     pool.query(queryText).then(( result) => {
-//         console.log(result.rows);//result is too big, we care about rows
-//         res.send(result.rows);
-//     }).catch((error) => {// catch if not working
-//         console.log('error in GET', error);
-//         //all good servers respond
-//         res.sendStatus(500);
-//     })
-//     //send songs back to client! 
-//     // res.send(tasks)
+// pool.on('connect', () =>{
+//     console.log('pg connected!');
 // });
 
-// router.post('/', (req, res) => {
-//     let queryText = `
-//     INSERT INTO "tasks" (INSERT INTO "tasks" ("actionItem", "levelOfImportance", "deadline", "complete", "additionalNotes")
-//     VALUES ($1, $2, $3, $4);
-//     `; //sanitization
-//     console.log(req.body);
-// })
+// pool.on('error', (error) =>{
+//     console.log('error with pool', error)
+// });
+
+//GET 
+taskRouter.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "tasks" ORDER BY "id";';
+    pool.query(queryText).then(result => {
+      res.send(result.rows);
+    })
+    .catch(error => {
+      console.log('error getting tasks', error);
+      res.sendStatus(500);
+    });
+})
+
+// POST
+taskRouter.post('/', (req, res) => {
+    let newTask  = req.body;
+    console.log('Adding new task:', newTask);
+    let queryTex = `
+    INSERT INTO "tasks" (INSERT INTO "tasks" ("actionItem", "levelOfImportance", "deadline", "complete", "additionalNotes")
+    VALUES ($1, $2, $3, $4);
+    `; //sanitization
+    pool.query(queryText, [newTask.actionItem, newTask.levelOfImportance, newTask.deadline, newTask.complete, newTask.additionalNotes])
+    .then(result => {
+      res.sendStatus(201);
+    })
+    .catch(error => {
+      console.log(`Error adding new task`, error); 
+      res.sendStatus(500);
+    });
+    
+});
 
 module.exports = tasksRouter;
